32a9beaeaa3dfd18cc9f01db6e2b7e691458c868
Rushyanth-29 <rushyanth.dodda@ext.robustsys-gmbh.de>
2023-07-21 04:12:50 +0530

    first

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define(
+    ["./BaseController"],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("com.personal.portfolio.controller.MainView", {
+            onHomePress: function () {
+                this.getOwnerComponent().getRouter().navTo("MainView");  
+              },
+            onpress1: function () {
+              this.getOwnerComponent().getRouter().navTo("Aboutme");  
+            },
+            onpress2: function () {
+                this.getOwnerComponent().getRouter().navTo("Skills");  
+            },
+            onpress3: function () {
+                this.getOwnerComponent().getRouter().navTo("Resume");  
+            },
+            onpress4: function () {
+                this.getOwnerComponent().getRouter().navTo("Contact");  
+            },
+        });
+    },
+);
